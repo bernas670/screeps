e6042b91e7e00b333312374394d42950e82d7b1b
Bernardo Santos <bernardosantos670@gmail.com>
2024-11-01 03:27:07 +0000

    Implemented routing in static-harvester so it sits on a container

--- src/roles/static-harvester.ts
+++ src/roles/static-harvester.ts
@@ -21,11 +21,13 @@ function findSource(creep: Creep): Source {
     const leastUsedSource = _.minBy(Object.keys(counter) as Id<Source>[], (source: Id<Source>) => counter[source]) as Id<Source>;
     const source = Game.getObjectById(leastUsedSource) as Source;
     creep.memory.source = source.id;
-    
+
     return source;
 }
 
 enum STATE {
+    ROUTING,
+    MOVING,
     COLLECTING,
 };
 
@@ -33,10 +35,35 @@ const StaticHarvester: Role = {
 
     run(creep: Creep) {
         if (!creep.memory.state) {
-            creep.memory.state = STATE.COLLECTING;
+            creep.memory.state = STATE.MOVING;
         }
 
         switch (creep.memory.state) {
+            case STATE.ROUTING:
+                const containers = findSource(creep).pos.findInRange(FIND_STRUCTURES, 1, {
+                    filter: (structure) => structure instanceof StructureContainer
+                });
+                if (containers.length)
+                    creep.memory.moveTo = containers[0].id;
+                creep.memory.state = STATE.MOVING;
+                break
+
+            case STATE.MOVING:
+                if (!creep.memory.moveTo) {
+                    creep.memory.state = STATE.COLLECTING;
+                    creep.memory.moveTo = undefined;
+                    break;
+                }
+
+                const target = Game.getObjectById(creep.memory.moveTo) as Structure;
+                if (creep.pos.isEqualTo(target.pos)) {
+                    creep.memory.state = STATE.COLLECTING;
+                    creep.memory.moveTo = undefined;
+                } else {
+                    creep.moveTo(target, { visualizePathStyle: { stroke: "#ffaa00" } });
+                }
+                break;
+
             case STATE.COLLECTING:
                 const source = findSource(creep);
                 if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
